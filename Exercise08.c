ba5d3719148617de8aad8c76cf180fa5fe76290f
NomNom-star <namuy24@gmail.com>
2024-11-22 08:22:41 +0700

    Update Exercise08.c

--- Exercise08.c
+++ Exercise08.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
 int main(){
-    int number, iNumber;
-    printf("Hãy nhập vào số nguyên có 4 chữ số\t");
-    scanf("%d", &number);
-    iNumber =  0 - number;
-    printf("Số nghịch đảo của số %d là %d", number, iNumber);
+    int number, num1, num2, num3, num4;
+    printf("Hay nhap vào 1 số nguyên có 4 chữ sô\t");
+    scanf("%d",&number);
+    num1= number/1000;
+    num2= (number/100) % 10;
+    num3= (number/10) % 10;
+    num4= number % 10;
+    
+    printf("Số nghịch đảo số %d là %d%d%d%d", number, num4, num3, num2, num1 );
+
 
     return 0;
 }
